076bf9290d917748667658c0d4dbe25d5d8c29a8
Julius Kanlapan <julius@3.digital>
2019-07-11 15:50:23 +0400

    test

--- storage/cms/cache/0e/b7/default.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5d270ee899709231531750_950e8bdac2ffe15ec837389bc7791c3fClass extends Cms\Classes\LayoutCode
-{
-
-}
--- storage/cms/cache/61/34/header.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5d270ee8a2932830179435_4e335fdcb4d856996f3142746dab96c6Class extends Cms\Classes\PartialCode
-{
-
-}
